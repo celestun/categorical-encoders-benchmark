1190ad04f8f326befc20d09c62b61ca5ff2af6ab
evaldez <ericvaldezvalenzuela@192.168.1.2>
2024-02-15 11:00:23 -0600

    remove files only if exists

--- entrypoint.py
+++ entrypoint.py
@@ -6,8 +6,10 @@ path_to_metadata = "./dataset_metadata"
 path_to_datasets = "./datasets"
 
 metadata_filenames = next(walk(path_to_metadata), (None, None, []))[2]  # [] if no file
-metadata_filenames.remove(".DS_Store")
-metadata_filenames.remove(".gitkeep")
+if ".DS_Store" in metadata_filenames:
+    metadata_filenames.remove(".DS_Store")
+if ".gitkeep" in metadata_filenames:
+    metadata_filenames.remove(".gitkeep")
 
 
 def main():
